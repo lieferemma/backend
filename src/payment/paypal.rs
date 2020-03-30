459c30f1ec562a352f89c9d0679a3e7f4830a8fe
marcelbuesing <marcel.buesing@codebrew.de>
2020-03-30 11:03:22 +0200

    Add paypal payment structs

--- src/payment/paypal.rs
+++ src/payment/paypal.rs
@@ -1,11 +1,13 @@
-// https://developer.paypal.com/docs/integration/direct/payments/paypal-payments/#request-1
-// curl -v -X GET https://api.sandbox.paypal.com/v1/payments/payment/PAY-1B56960729604235TKQQIYVY \
-//   -H "Content-Type: application/json" \
-//   -H "Authorization: Bearer Access-Token"
+//! https://developer.paypal.com/docs/checkout/reference/server-integration/get-transaction/
+//! https://developer.paypal.com/docs/integration/direct/payments/paypal-payments/#request-1
+//! curl -v -X GET https://api.sandbox.paypal.com/v1/payments/payment/PAY-1B56960729604235TKQQIYVY \
+//!   -H "Content-Type: application/json" \
+//!   -H "Authorization: Bearer Access-Token"
 
-use url::{Url};
 use anyhow::{anyhow, Result};
+use serde::Deserialize;
 use std::str::FromStr;
+use url::Url;
 
 #[cfg(test)]
 mod tests {
@@ -14,9 +16,18 @@ mod tests {
     #[test]
     fn verify_transaction_id() {
         assert!(matches!(TransactionId::from_str("PAY-TOOSHORT"), Err(_)));
-        assert!(matches!(TransactionId::from_str("PAY-TOOLONGSTRINGNOTFITTINGAVALIDTRANSACTIONID"), Err(_)));
-        assert!(matches!(TransactionId::from_str("PAY-1B56960729604235TKQQIY/Y"), Err(_)));
-        assert!(matches!(TransactionId::from_str("PAY-1B56960729604235TKQQIYVY"), Ok(_)));
+        assert!(matches!(
+            TransactionId::from_str("PAY-TOOLONGSTRINGNOTFITTINGAVALIDTRANSACTIONID"),
+            Err(_)
+        ));
+        assert!(matches!(
+            TransactionId::from_str("PAY-1B56960729604235TKQQIY/Y"),
+            Err(_)
+        ));
+        assert!(matches!(
+            TransactionId::from_str("PAY-1B56960729604235TKQQIYVY"),
+            Ok(_)
+        ));
     }
 }
 
@@ -28,15 +39,22 @@ impl FromStr for TransactionId {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.len() != 28 {
-            return Err(anyhow!("Invalid transaction id length {} != 28: {}", s.len(), s));
+            return Err(anyhow!(
+                "Invalid transaction id length {} != 28: {}",
+                s.len(),
+                s
+            ));
         }
         if &s[0..=3] != "PAY-" {
             return Err(anyhow!("Invalid prefix of transaction id: {}", s));
         }
 
-        for c in s[4..=27].chars(){
+        for c in s[4..=27].chars() {
             if !c.is_digit(10) && !c.is_ascii_uppercase() {
-                return Err(anyhow!("Invalid character in suffix of transaction id: {}", s));
+                return Err(anyhow!(
+                    "Invalid character in suffix of transaction id: {}",
+                    s
+                ));
             }
         }
 
@@ -44,10 +62,52 @@ impl FromStr for TransactionId {
     }
 }
 
+#[derive(Deserialize)]
+struct Payment {
+    #[serde(rename = "id")]
+    id: String,
+    #[serde(rename = "create_time")]
+    create_time: String,
+    #[serde(rename = "update_time")]
+    update_time: String,
+    #[serde(rename = "state")]
+    state: String,
+    #[serde(rename = "intent")]
+    intent: String,
+    #[serde(rename = "payer")]
+    payer: Payer,
+    #[serde(rename = "transactions")]
+    transactions: Vec<Transaction>,
+}
+
+#[derive(Deserialize)]
+struct Payer {
+    #[serde(rename = "payment_method")]
+    payment_method: String,
+}
+
+#[derive(Deserialize)]
+struct Money {
+    #[serde(rename = "currency_code")]
+    currency_code: String,
+    #[serde(rename = "value")]
+    value: String,
+}
+
+#[derive(Deserialize)]
+struct Transaction {
+    #[serde(rename = "amount")]
+    amount: Money,
+    #[serde(rename = "description")]
+    description: String,
+    #[serde(rename = "note_to_payer")]
+    note_to_payer: String,
+}
+
 // fn verify_transaction(transaction_id: TransactionId) -> Result<()> {
 //     let paypal_api = Url::parse("https://api.sandbox.paypal.com/v1/payments/payment/")?;
-//     let paypal_url = paypal_api.join(transaction_id);
-//     let body = reqwest::get("https://www.rust-lang.org")
+//     let paypal_url = paypal_api.join(&transaction_id.0);
+//     let body = reqwest::get(paypal_url)
 //     .await?
 //     .text()
 //     .await?;
