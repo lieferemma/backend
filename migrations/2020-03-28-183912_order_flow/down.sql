539b8ae2cf21d79dc1d54faa4f4b35e00f8c1f1b
a.el-hindi <ahelhindi@gmail.com>
2020-04-02 20:14:40 +0200

    Save current state

--- migrations/2020-03-28-183912_order_flow/down.sql
+++ migrations/2020-03-28-183912_order_flow/down.sql
@@ -1,1 +1,11 @@
--- This file should undo anything in `up.sql`
+DROP TABLE orders;
+
+DROP TYPE order_status;
+
+DROP TYPE papayment_statusckaging_unit;
+
+DROP TABLE oder_status_updates;
+
+DROP TABLE payment_status;
+
+DROP TABLE order_items;
--- migrations/2020-03-28-183912_order_flow/up.sql
+++ migrations/2020-03-28-183912_order_flow/up.sql
@@ -1,25 +1,4 @@
 
-
-CREATE TABLE orders (
-    order_uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    -- human readable order id
-    order_id VARCHAR(1024) NOT NULL,
-    -- Shop that will receive the payment
-    mobile_shop_uuid UUID REFERENCES mobile_shops(mobile_shop_id),
-    
-
-    oder_status oder_status REFERENCES oder_status_updates(order_uuid)
-
-    delivery_point_id UUID REFERENCES delivery_points(delivery_point_id)
-   -- Currency to display
-    currency currency_codes,
-    -- calculated total of the order
-    total BIGINT NOT NULL,
-    -- Items that have been ordered by the customer
-    ordered_products REFERENCES order_items(order_uuid)
- 
-
-);
 -- maps to shipment status in proto messages
 CREATE TYPE order_status AS ENUM ('CREATED', 'LOADED', 'SENT', 'COLLECTED');
 
@@ -27,28 +6,56 @@ CREATE TYPE payment_status AS ENUM ('NOTPAYED', 'PAYED');
 
 
 CREATE TABLE oder_status_updates (
-    order_uuid UUID REFERENCES orders,
+    order_uuid UUID NOT NULL REFERENCES orders,
 
     update_time TIMESTAMPTZ NOT NULL,
+
     current_status order_status,
+
     PRIMARY KEY (order_uuid)
-
-
-
 );
 
 CREATE TABLE payment_status (
-    order_uuid UUID REFERENCES orders,
-    update_time TIMESTAMPTZ NOT NULL,
-    current_status payment_status
-    PRIMARY KEY (order_uuid)
+    order_uuid UUID NOT NULL REFERENCES orders,
 
+    update_time TIMESTAMPTZ NOT NULL,
+
+    current_status payment_status,
+
+    PRIMARY KEY (order_uuid)
 );
 
 
 CREATE TABLE order_items (
-    product_id UUID REFERENCES products ON DELETE RESTRICT,
-    order_uuid UUID REFERENCES orders ON DELETE CASCADE,
-    quantity integer,
+    product_id UUID NOT NULL REFERENCES products ON DELETE RESTRICT,
+
+    order_uuid UUID NOT NULL REFERENCES orders ON DELETE CASCADE,
+
+    quantity BIGINT NOT NULL,
+
     PRIMARY KEY (product_id, order_uuid)
 );
+
+CREATE TABLE orders (
+    order_uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    -- human readable order id
+    order_id VARCHAR(1024) NOT NULL,
+    -- Shop that will receive the payment
+    
+    mobile_shop_uuid UUID NOT NULL REFERENCES mobile_shops(mobile_shop_id),
+
+
+    oder_status oder_status NOT NULL REFERENCES oder_status_updates(order_uuid),
+
+    delivery_point_id UUID NOT NULL REFERENCES delivery_points(delivery_point_id),
+
+    
+   -- Currency to display
+    currency currency_codes,
+    -- calculated total of the order
+    total BIGINT NOT NULL,
+    -- Items that have been ordered by the customer
+    ordered_products NOT NULL REFERENCES order_items(order_uuid)
+    
+);
+
