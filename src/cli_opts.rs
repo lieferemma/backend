492f365d2920695e7bd4bb6f949cdb7cc705436d
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2020-04-06 15:53:05 +0000

    Resolve grpc addr resource conflict

--- src/cli_opts.rs
+++ src/cli_opts.rs
@@ -4,7 +4,7 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 pub struct Opt {
     /// Address to bind gRPC api to.
-    #[structopt(long, env = "GRPC_API_ADDR", default_value = "0.0.0.0:50051")]
+    #[structopt(long, env = "GRPC_API_ADDR", default_value = "0.0.0.0:50052")]
     grpc_api_addr: SocketAddr,
 
     /// Postgres database url
